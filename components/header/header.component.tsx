85e5c7f6c50b750b2a3e762cd5196e4d3418d78c
Ssen Galanto <galanto@mclinica.com>
2020-09-15 21:36:03 +0800

    chore: minor fix finished landing page design

--- components/header/header.component.tsx
+++ components/header/header.component.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC = () => {
       <div className="container mx-auto p-5 md:py-3 flex justify-between items-center">
         <img src="/images/logo.svg" alt="Easybank" />
         <img className="h-full d md:hidden" src="/images/icon-hamburger.svg" alt="Burger Icon" />
-        <nav className='hidden md:block'>
+        <nav className='hidden md:inline-block'>
           {menu.map((path) => (
             <Link href="/" key={path}>
               <a className="text-sm font-normal text-tertiary mr-6">{path}</a>
